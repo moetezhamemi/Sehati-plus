ed62598ec4c90538e90015431ffc00de2835533c
Moetez Hamemi <moetezhamemi18@gmail.com>
2026-03-11 11:17:28 +0100

    Configuration initiale du projet

---
+++ sehati/src/main/java/com/sehati/SehatiApplication.java
@@ -0,0 +1,13 @@
+package com.sehati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SehatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SehatiApplication.class, args);
+	}
+
+}
